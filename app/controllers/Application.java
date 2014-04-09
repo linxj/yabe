11e322e57158828059d6b1b9fd2ae97a6f6aafb1
linxj <linxj92@gmail.com>
2014-04-09 14:58:50 +0200

    update on 09.04.2014 at 14:58

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,5 +16,11 @@ public class Application extends Controller {
         ).from(1).fetch(10);
         render(frontPost, olderPosts);
     }
+    
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
  
 }
